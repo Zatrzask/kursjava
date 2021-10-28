239a00ccd6ce3983cf1bfa3be9448dbdc6435a6b
Zatrzask <marcinklink@gmail.com>
2021-10-28 17:31:23 +0200

    chuck noris jokes

---
+++ src/main/java/pl/teslachorzow/apierwszyprogram/Main03.java
@@ -0,0 +1,15 @@
+package pl.teslachorzow.apierwszyprogram;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class Main03 {
+    public static void main(String[] args) {
+        ChuckNorrisQuotes jokes = new ChuckNorrisQuotes();
+        System.out.print("1-");
+        System.out.println(jokes.getRandomQuote());
+        System.out.print("2-");
+        System.out.println(jokes.getRandomQuote());
+        System.out.print("3-");
+        System.out.println(jokes.getRandomQuote());
+    }
+}
