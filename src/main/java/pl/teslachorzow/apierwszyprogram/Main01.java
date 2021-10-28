0d4cd31138552c6cbf1d6e19583d3cb86eaefc2c
Zatrzask <marcinklink@gmail.com>
2021-10-28 16:57:25 +0200

    moj pierwszy commit

---
+++ src/main/java/pl/teslachorzow/apierwszyprogram/Main01.java
@@ -0,0 +1,13 @@
+package pl.teslachorzow.apierwszyprogram;
+
+public class Main01 {
+    // psvm
+    public static void main(String[] args) {
+       // sout
+        System.out.print("halo halo");
+        System.out.println("Mój pierwszy program w Java"); System.out.print("efiwerfoijfoio");
+
+
+
+    }
+    }
